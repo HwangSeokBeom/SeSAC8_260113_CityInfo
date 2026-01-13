db5c824ce3730c6caea5c120f6239bf26dd0b179
HwangSeokBeom <tjrqja0714@gmail.com>
2026-01-13 16:11:11 +0900

    Initial Commit

---
+++ SeSAC8_260113_CityInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC8_260113_CityInfo
+//
+//  Created by Hwangseokbeom on 1/13/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
